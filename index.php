68a0989d19fa5587a864fbc9637bf540e9279d7a
et6984 <e69t84@gmail.com>
2025-03-20 11:10:47 +0100

    image

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css?version=<?php echo filemtime('css/style.css'); ?>">
+    <title>Capteur d'humidité</title>
+</head>
+<body>
+    <header>
+        <img src="image/logo.png" alt="logo" id="logo">
+        <h1>Capteur d'humidité</h1>
+    </header>
+    <main>
+        <div id="div-valeur">
+            <div class="valeur">
+
+            </div>
+            <div class="valeur">
+                
+            </div>
+        </div>
+        <div id="div-graphique">
+        </div>    
+    </main>
+</body>
+</html>
