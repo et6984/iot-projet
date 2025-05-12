d701e538d407dc439f3db37089fa7a79f8e321b5
et6984 <e69t84@gmail.com>
2025-05-12 11:34:27 +0200

    modification requete insert donnée

--- php/data_test.php
+++ php/data_test.php
@@ -1,26 +1,28 @@
 <?php
 session_start();
 $host = "localhost";
-$db = "salle_serveur";
+$db = "projet-iot";
 $user = "capteur";
 $password = "password";
 
 $temp = $_GET['temp'];
 $humi = $_GET['humi'];
+$id_salle = $_GET['id_salle'];
 
-if (!empty($temp) || !empty($humi)) {
+if (!empty($temp) && !empty($humi) && !empty($id_salle)) {
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
         $stmt = $pdo->prepare("
-            INSERT INTO `capteur`(`TYPE_CAPTEUR`, `MESURE`) 
+            INSERT INTO `capteur`(`ID_SALLE`, `TYPE_CAPTEUR`, `MESURE`) 
             VALUES 
-            ('T', :temp),
-            ('H', :humi)
+            (:id_salle, 'T', :temp),
+            (:id_salle, 'H', :humi);
         ");
         $stmt->bindParam(':temp', $temp, PDO::PARAM_STR);
         $stmt->bindParam(':humi', $humi, PDO::PARAM_STR);
+        $stmt->bindParam(':id_salle', $id_salle, PDO::PARAM_STR);
         $stmt->execute();
         
     } catch (PDOException $e) {
